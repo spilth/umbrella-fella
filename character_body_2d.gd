5b54ab523fb2fef3b75bd5c6f660c35ea0fd7562
Brian Kelly <polymonic@gmail.com>
2024-02-24 15:28:45 -0600

    Fix animations, add music, main menu and ending scene

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 250.0
+const JUMP_VELOCITY = -450.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var jump_sound = $JumpSound
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,16 +17,16 @@ func _physics_process(delta):
 			animated_sprite_2d.animation = "floating"
 			velocity.y += gravity / 16 * delta
 		else:
-			animated_sprite_2d.animation = "default"
+			animated_sprite_2d.animation = "jumping"
 			velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 				
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -37,13 +37,18 @@ func _physics_process(delta):
 		if Input.is_action_pressed("Block"):
 			animated_sprite_2d.animation = "blocking"
 		elif velocity.x != 0:
-			animated_sprite_2d.animation = "default"
-
-
+			animated_sprite_2d.animation = "running"
+		else:
+			animated_sprite_2d.animation = "idle"
+	
 	move_and_slide()
+
 	
 	if velocity.x > 0:
 		animated_sprite_2d.flip_h = false
+
 	
 	if velocity.x < 0:
 		animated_sprite_2d.flip_h = true
+
+
