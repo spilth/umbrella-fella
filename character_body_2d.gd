4d3570bd07018431770ddc6a8ea91dfc3ad6ecdb
Brian Kelly <polymonic@gmail.com>
2024-02-25 17:30:30 -0600

    Add floating and jump sound. Start replacing graphics

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -6,13 +6,19 @@ const JUMP_VELOCITY = -450.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var jump_sound = $JumpSound
+@onready var float_sound = $FloatSound
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta):
-	# Add the gravity.
+func _physics_process(delta):		
 	if not is_on_floor():
+		if Input.is_action_just_pressed("Open Umbrella"):
+			float_sound.play()
+			
+		if Input.is_action_just_released("Open Umbrella"):
+			float_sound.stop()
+
 		if Input.is_action_pressed("Open Umbrella") && velocity.y > 0:
 			animated_sprite_2d.animation = "floating"
 			velocity.y = gravity * 4 * delta
@@ -22,8 +28,8 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 		jump_sound.play()
+		velocity.y = JUMP_VELOCITY
 		
 	if Input.is_action_just_pressed("Down") and is_on_floor():
 		position.y += 3
@@ -37,6 +43,9 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	if is_on_floor():
+		if float_sound.playing:
+			float_sound.stop()
+			
 		if Input.is_action_pressed("Block"):
 			animated_sprite_2d.animation = "blocking"
 		elif velocity.x != 0:
