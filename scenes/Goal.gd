15381cf169199a7d9065a59c8f2067dcce2b43d8
Brian Kelly <polymonic@gmail.com>
2024-02-25 17:39:40 -0600

    Make exit scene customizable and defer call to change it

--- scenes/Goal.gd
+++ scenes/Goal.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@export var destination : PackedScene
+
 func _on_body_entered(body):
 	if (body.name == "Player"):
-		get_tree().change_scene_to_file("res://ending.tscn")
+		call_deferred("load_level")
+
+func load_level():
+	get_tree().change_scene_to_packed(destination)
