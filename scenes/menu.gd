c514b4ae72403a71f1434abf3d65ced1ca4e10e1
Brian Kelly <polymonic@gmail.com>
2024-02-26 14:10:47 -0600

    Add keys for buttons

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -1,4 +1,12 @@
 extends Node
 
+@onready var play_button = $PlayButton
+
+func _ready():
+	$PlayButton.grab_focus()
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
