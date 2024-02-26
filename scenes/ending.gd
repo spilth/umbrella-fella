01b80b06a47fc208b2a71027296854e4115251d9
Brian Kelly <polymonic@gmail.com>
2024-02-26 08:33:07 -0600

    Scene cleanup and renaming

--- scenes/ending.gd
+++ scenes/ending.gd
@@ -1,4 +1,7 @@
 extends Node
 
-func _on_button_pressed():
+func _on_play_again_pressed():
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
