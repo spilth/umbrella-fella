b167476ff409f3af73ece54c4c3cf52cc1985588
Brian Kelly <polymonic@gmail.com>
2024-02-26 13:39:34 -0600

    Fix coin pickup warning

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -7,9 +7,11 @@ extends Area2D
 func _on_body_entered(body):
 	if (body.name == "Player"):
 		audio_stream_player_2d.play()
-		sprite_2d.visible = false
-		collision_shape_2d.disabled = true
+		call_deferred("pickup")
 
+func pickup():
+	sprite_2d.visible = false
+	collision_shape_2d.disabled = true
 
 func _on_audio_stream_player_2d_finished():
 	queue_free()
