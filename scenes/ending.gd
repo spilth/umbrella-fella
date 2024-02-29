338528fde2c42d47a8a9c17cb821657d6c6ef18b
Brian Kelly <polymonic@gmail.com>
2024-02-28 20:36:23 -0600

    Use autoloaded scene to control the music

--- scenes/ending.gd
+++ scenes/ending.gd
@@ -1,7 +1,13 @@
 extends Node
 
+func _ready():
+	Autoloaded.get_node("Levels").stop()
+	Autoloaded.get_node("Ending").play()
+
 func _on_play_again_pressed():
+	Autoloaded.get_node("Levels").play()
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
 
 func _on_main_menu_pressed():
+	Autoloaded.get_node("Ending").stop()
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
--- scenes/menu.gd
+++ scenes/menu.gd
@@ -4,9 +4,13 @@ extends Node
 
 func _ready():
 	$PlayButton.grab_focus()
+	Autoloaded.get_node("Menu").play()
 
 func _on_play_button_pressed():
+	Autoloaded.get_node("Menu").stop()
+	Autoloaded.get_node("Levels").play()
 	get_tree().change_scene_to_file("res://scenes/level0.tscn")
+	
 
 func _on_quit_button_pressed():
 	get_tree().quit()
