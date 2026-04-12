e1739b0306d560bf5715674db4b1bbaeaa9a4ca3
Brian Kelly <brian@spilth.org>
2026-04-12 14:22:49 -0500

    Play Again should link to level 0, not level 1

--- scenes/ending.gd
+++ scenes/ending.gd
@@ -6,7 +6,7 @@ func _ready():
 
 func _on_play_again_pressed():
 	Autoloaded.get_node("Levels").play()
-	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level0.tscn")
 
 func _on_main_menu_pressed():
 	Autoloaded.get_node("Ending").stop()
